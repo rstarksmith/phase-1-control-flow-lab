24138cb33b096584c0dc3ff4769fc6e087e20793
Rachel <rstark.smith@gmail.com>
2022-03-16 23:16:36 -0500

    commit tree 394748912a27cf305539b6c754a195145c714781

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return 'This one is on me!';
+  } else if (someValue >= 2001 && someValue <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (someValue >= 2501){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+  }
 }
